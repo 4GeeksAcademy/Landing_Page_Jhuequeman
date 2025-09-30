d0e9dbe755a669eb432d017e17492526b02b3d62
jhuequeman <huequeman@gmail.com>
2025-09-30 00:40:11 +0000

    Landig_page_jhuequeman

---
+++ src/js/components/Card.jsx
@@ -0,0 +1,16 @@
+const Card = (props) => {
+  return (
+        <div className="card" style={{ width: "18rem" }}>
+          <img src= {props.imgUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title.
+            </p>
+            <a href="#" className="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+  );
+};
+
+export default Card;
---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,12 @@
+const Jumbotron = () =>{
+    return(
+        <div className="p-5 mb-4 bg-body-tertiary rounded-3"> 
+            <div className="container-fluid py-5"> 
+            <h1 className="display-5 fw-bold">A Warm Welcome</h1> 
+            <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, maxime dolor tempore nisi minima expedita corporis, ipsam aliquam soluta quasi optio sint alias, hic voluptas explicabo accusantium illum. Amet, asperiores?.</p> 
+            <button className="btn btn-primary btn-lg" type="button">Call to action!</button> </div> 
+            </div>
+    )
+}
+
+export default Jumbotron;
